c3455b007ee0bc90901a2c2b4effa7a5ffe7ba09
Melissa Peters <melissap@nulogy.com>
2024-12-04 14:32:57 -0500

    day-4: Refacotr: Remove duplicate code

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,5 +1,8 @@
 import { Day } from "../day";
 
+type Direction = 'Up' | 'Down' | 'Left' | 'Right' | 'Up-Left' | 'Up-Right' | 'Down-Left' | 'Down-Right';
+
+const DIRECTIONS: Direction[] = ['Up', 'Down', 'Left', 'Right', 'Up-Left', 'Up-Right', 'Down-Left', 'Down-Right'];
 class Day4 extends Day {
 
     constructor(){
@@ -12,17 +15,9 @@ class Day4 extends Day {
         for (let row = 0; row < grid.length; row++) {
             for (let col = 0; col < grid[row].length; col++) {
                 if (grid[row][col] === 'X') {
-                    // check the 8 surrounding characters
-                    if(this.checkSurroundingCharacters(grid, row+1, col, "Down")) xmas_count++;
-                    if(this.checkSurroundingCharacters(grid, row+1, col+1, "Down-Right")) xmas_count++;
-                    if(this.checkSurroundingCharacters(grid, row+1, col-1, "Down-Left")) xmas_count++;
-
-                    if(this.checkSurroundingCharacters(grid, row-1, col, "Up")) xmas_count++;
-                    if(this.checkSurroundingCharacters(grid, row-1, col+1, "Up-Right")) xmas_count++;
-                    if(this.checkSurroundingCharacters(grid, row-1, col-1, "Up-Left")) xmas_count++;
-
-                    if(this.checkSurroundingCharacters(grid, row, col+1, "Right")) xmas_count++;
-                    if(this.checkSurroundingCharacters(grid, row, col-1, "Left")) xmas_count++;
+                    DIRECTIONS.forEach((direction) => {
+                        if(this.checkSurroundingCharacters(grid, row, col, direction)) xmas_count++;
+                    });
                 }
             }
         }
@@ -31,8 +26,7 @@ class Day4 extends Day {
     }
 
     checkSurroundingCharacters(grid: string[][], row: number, col: number, direction: string): boolean {
-        let x = row;
-        let y = col;
+        let [x,y] = this.moveInDirection(row, col, direction);
         let found = true;
         const letters = ['M', 'A', 'S'];
 
@@ -41,27 +35,7 @@ class Day4 extends Day {
                 found = false;
             }
 
-            if (direction === 'Up') {
-                x--;
-            } else if (direction === 'Down') {
-                x++;
-            } else if (direction === 'Left') {
-                y--;
-            } else if (direction === 'Right') {
-                y++;
-            } else if (direction === 'Up-Left') {
-                x--;
-                y--;
-            } else if (direction === 'Up-Right') {
-                x--;
-                y++;
-            } else if (direction === 'Down-Left') {
-                x++;
-                y--;
-            } else if (direction === 'Down-Right') {
-                x++;
-                y++;
-            }
+            [x,y] = this.moveInDirection(x, y, direction);
         });
 
         return found;
@@ -116,35 +90,36 @@ class Day4 extends Day {
         return input.split('\n').map((line) => line.split(''));
     }
 
-    getValueInDirection(grid: string[][], row: number, col: number, direction: string) {
-        let [x, y] = [row, col];
-        if (direction === 'Up') {
-            x--;
-        } else if (direction === 'Down') {
-            x++;
-        } else if (direction === 'Left') {
-            y--;
-        } else if (direction === 'Right') {
-            y++;
-        } else if (direction === 'Up-Left') {
-            x--;
-            y--;
-        } else if (direction === 'Up-Right') {
-            x--;
-            y++;
-        } else if (direction === 'Down-Left') {
-            x++;
-            y--;
-        } else if (direction === 'Down-Right') {
-            x++;
-            y++;
-        }
+    getValueInDirection(grid: string[][], row: number, col: number, direction: Direction) {
+        let [x, y] = this.moveInDirection(row, col, direction);
 
         if(x >= 0 && x < grid.length && y >= 0 && y < grid[x].length) {
             return grid[x][y];
         }
 
         return "";
+    }
+
+    moveInDirection(row: number, col: number, direction: string): [number, number] {
+        if (direction === 'Up') {
+            return [row - 1, col];
+        } else if (direction === 'Down') {
+            return [row + 1, col];
+        } else if (direction === 'Left') {
+            return [row, col - 1];
+        } else if (direction === 'Right') {
+            return [row, col + 1];
+        } else if (direction === 'Up-Left') {
+            return [row - 1, col - 1];
+        } else if (direction === 'Up-Right') {
+            return [row - 1, col + 1];
+        } else if (direction === 'Down-Left') {
+            return [row + 1, col - 1];
+        } else if (direction === 'Down-Right') {
+            return [row + 1, col + 1];
+        }
+
+        return [row, col];
     }
 }
 
