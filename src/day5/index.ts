c81a4fab11621c909b343e6d4a362c08a4e38f8e
Melissa Peters <melissap@nulogy.com>
2024-12-07 18:24:41 -0500

    day-5: Refactor: Remove duplication

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,50 +1,46 @@
 import { Day } from '../day'
 
 class Rule {
-    firstNumber: number;
-    secondNumber: number;
-    constructor (firstNumber: number, secondNumber: number) {
-      this.firstNumber = firstNumber
-      this.secondNumber = secondNumber
-    }
+  firstNumber: number;
+  secondNumber: number;
+  constructor (firstNumber: number, secondNumber: number) {
+    this.firstNumber = firstNumber
+    this.secondNumber = secondNumber
+  }
 
-    public passesRuleAt (index: number, update: number[]) : boolean {
-      // Assume that we mapped the rules only to the first number
-      // That means it needs to be first so make sure the second number is not before it
-      let pass = true
-      for (let i = 0; i < index; i++) {
-        if (update[i] === this.secondNumber) {
-          pass = false
-          break
-        }
+  public passesRuleAt (index: number, update: number[]) : boolean {
+    let pass = true
+    for (let i = 0; i < index; i++) {
+      if (update[i] === this.secondNumber) {
+        pass = false
+        break
       }
-
-      return pass
     }
 
-    public failingIndex (index: number, update: number[]) : number {
-    // Assume that we mapped the rules only to the first number
-    // That means it needs to be first so make sure the second number is not before it
-      let failingIndex = -1;
-      for (let i = 0; i < index; i++) {
-        if (update[i] === this.secondNumber) {
-          failingIndex = i;
-          break;
-        }
+    return pass
+  }
+
+  public failingIndex (index: number, update: number[]) : number {
+    let failingIndex = -1;
+    for (let i = 0; i < index; i++) {
+      if (update[i] === this.secondNumber) {
+        failingIndex = i;
+        break;
       }
-
-      return failingIndex;
     }
+
+    return failingIndex;
+  }
 }
+
 class Day5 extends Day {
   constructor () {
     super(5)
     this.expectedResultPart1 = '4462'
-    this.expectedResultPart2 = '?'
+    this.expectedResultPart2 = '6767'
   }
 
-  solveForPartOne (input: string): string {
-    // Read the rules one by one
+  private parseInput (input: string): { rules: Map<number, Rule[]>, updates: number[][] } {
     let readingRules = true
     const rules = new Map<number, Rule[]>()
     const updates: number[][] = []
@@ -64,20 +60,20 @@ class Day5 extends Day {
         updates.push(x.split(',').map(Number))
       }
     })
+    return { rules, updates }
+  }
 
+  solveForPartOne (input: string): string {
+    const { rules, updates } = this.parseInput(input)
     let result = 0
 
     updates.forEach((update) => {
       if (this.checkRules(rules, update)) {
-        // find the middle and add it to results
         const middle = Math.floor(update.length / 2)
         result += update[middle]
       }
     })
 
-    // Go though pages, get rules for current number,
-    //  do they all pass? Continue to next number
-    // else false
     return result.toString()
   }
 
@@ -98,45 +94,20 @@ class Day5 extends Day {
   }
 
   solveForPartTwo (input: string): string {
-    // Read the rules one by one
-    let readingRules = true
-    const rules = new Map<number, Rule[]>()
-    const updates: number[][] = []
-    input.split('\n').forEach((x) => {
-      if (x === '') {
-        readingRules = false
-      } else if (readingRules) {
-        const [firstPage, secondPage] = x.split('|').map(Number)
-        const rule = new Rule(firstPage, secondPage)
-
-        if (!rules.has(firstPage)) {
-          rules.set(firstPage, [rule])
-        } else {
-          rules.get(firstPage)!.push(rule)
-        }
-      } else {
-        updates.push(x.split(',').map(Number))
-      }
-    })
-
+    const { rules, updates } = this.parseInput(input)
     let result = 0
 
     updates.forEach((update) => {
       if (!this.checkRules(rules, update)) {
-        // fix it?
         let fixedUpdate = update
         while (!this.checkRules(rules, fixedUpdate)) {
           fixedUpdate = this.fixUpdate(rules, fixedUpdate)
         }
-        // find the middle and add it to results
         const middle = Math.floor(fixedUpdate.length / 2)
         result += fixedUpdate[middle]
       }
     })
 
-    // Go though pages, get rules for current number,
-    //  do they all pass? Continue to next number
-    // else false
     return result.toString()
   }
 
